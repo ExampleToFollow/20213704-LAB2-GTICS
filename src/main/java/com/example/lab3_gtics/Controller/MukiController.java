5962c3ce5072744d24b2ecbc426379528f8180ee
Hineill <hineill.cespedes@pucp.edu.pe>
2024-04-20 15:23:59 -0500

    Mejorando parte visual

--- src/main/java/com/example/lab3_gtics/Controller/MukiController.java
+++ src/main/java/com/example/lab3_gtics/Controller/MukiController.java
@@ -51,13 +51,6 @@ public class MukiController {
             matriz[listaNum.get(2*i-2)][listaNum.get(2*i-1)] = 20;
         }
 
-        /*
-        for(int i=0;  i<mina.getNumFilas(); i++){
-            for(int j=0;  j<mina.getNumColumnas(); j++) {
-                System.out.println(matriz[i][j]);
-            }
-        }*/
-
         int counter= 0;
         for(int i=0;  i<mina.getNumFilas(); i++){
             for(int j=0;  j<mina.getNumColumnas(); j++){
@@ -77,6 +70,9 @@ public class MukiController {
                     }
                     matriz[i][j] = counter;
                 }
+                if(matriz[i][j]==0){
+                    matriz[i][j] = 30;
+                }
             }
         }
         mina.setPosiciones(matriz);
@@ -89,14 +85,6 @@ public class MukiController {
             }
         }
         mina.setEstadoMina(matrizEstado);
-
-        /*
-        for(int i=0;  i<mina.getNumFilas(); i++){
-            for(int j=0;  j<mina.getNumColumnas(); j++) {
-                System.out.println(matriz[i][j]);
-            }
-        }
-*/
 
         model.addAttribute("mina",mina);
         return "juego";
@@ -112,29 +100,44 @@ public class MukiController {
             caracter = posicion.charAt(i);
             try{
                 listaNum.add(Integer.parseInt(String.valueOf(caracter))-1);
-                //System.out.println(caracter);
             }catch(NumberFormatException e) {
                 continue;
             }
         }
+
+
         int fila = listaNum.get(0);
         int columna = listaNum.get(1);
+        int[][] matriz = mina.getPosiciones();
 
+        if(mina.getPosiciones()[fila][columna] !=20 ) {
+            //No escoge bomba
+            //Recorremos alrededor de la casilla escogida
+            matriz[fila][columna]= matriz[fila][columna]*(-1);
+            for(int i= fila-1 ; i<=fila+1; i++ ){
+                for(int j= columna-1 ; j<=columna+1 ; j++){
+                    //Restriccion para los bordes de la matriz
+                    if((i>=0 && i<= mina.getNumFilas()-1) && (j>=0 && j<=mina.getNumColumnas()-1) && (i!=fila || j!=columna)){
+                        //Restriccion para detectar otra bomba
+                        if(matriz[i][j] >0 && matriz[i][j] !=20 ){
+                            matriz[i][j] =matriz[i][j]*(-1);
+                        }
+                    }
+                }
+            }
 
-
-        int [][] matriz = mina.getPosiciones();
-        String [][] matrizEstado =  mina.getEstadoMina();
-        int counter= 0;
-        for(int i=0;  i<mina.getNumFilas(); i++){
-            for(int j=0;  j<mina.getNumColumnas(); j++){
-                if(matriz[i][j]!=20) {
-                    //Recorremos elementos contiguos
-                    for (int k = i - 1; k <= i + 1; k++) {
-                        for (int h = j - 1; h <= j + 1; h++) {
-                            if ((k >= 0 && k <= mina.getNumFilas() - 1) && (h >= 0 && h <= mina.getNumFilas() - 1)) {
-                                if (h != k) {
-                                    if (k==fila && h==columna ) {
-                                        matrizEstado[i][j]="Descubierto";
+            /*
+            for (int i = 0; i < mina.getNumFilas(); i++) {
+                for (int j = 0; j < mina.getNumColumnas(); j++) {
+                    if (matriz[i][j] != 20) {
+                        //Recorremos elementos contiguos
+                        for (int k = i - 1; k <= i + 1; k++) {
+                            for (int h = j - 1; h <= j + 1; h++) {
+                                if ((k >= 0 && k <= mina.getNumFilas() - 1) && (h >= 0 && h <= mina.getNumFilas() - 1)) {
+                                    if (h != k) {
+                                        if (k == fila && h == columna) {
+                                            matriz[k][h] = matriz[k][h]*(-1);
+                                        }
                                     }
                                 }
                             }
@@ -142,10 +145,20 @@ public class MukiController {
                     }
                 }
             }
+            */
+
+            mina.setPosiciones(matriz);
+
+        }else{
+            //Escoge bomba
+            matriz[fila][columna] = matriz[fila][columna]*(-1);
+            mina.setPosiciones(matriz);
+            mina.setFallas(mina.getFallas()+1);
+            if(mina.getFallas()>=mina.getNumIntentos()){
+                mina.setPerdio(true);
+            }
         }
 
-
-        mina.setEstadoMina(matrizEstado);
         //LLAMEN A DIOS
         return "juego";
     }
