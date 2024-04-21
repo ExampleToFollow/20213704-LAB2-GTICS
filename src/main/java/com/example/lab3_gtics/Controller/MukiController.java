320f1a7ca76e1cf0f8fdac30cff6af2e033a85fb
Hineill <hineill.cespedes@pucp.edu.pe>
2024-04-20 20:10:18 -0500

    :D

--- src/main/java/com/example/lab3_gtics/Controller/MukiController.java
+++ src/main/java/com/example/lab3_gtics/Controller/MukiController.java
@@ -53,7 +53,6 @@ public class MukiController {
 
         int counter= 0;
 
-
         for(int i=0;  i<mina.getNumFilas(); i++){
             for(int j=0;  j<mina.getNumColumnas(); j++){
                 if(matriz[i][j]!=20) {
@@ -62,17 +61,16 @@ public class MukiController {
                     for (int k = i - 1; k <= i + 1; k++) {
                         for (int h = j - 1; h <= j + 1; h++) {
                             if ((k >= 0 && k <= mina.getNumFilas() - 1) && (h >= 0 && h <= mina.getNumColumnas() - 1) ) {
-                                if (h != i || k !=j) {
+
                                     if (matriz[k][h] == 20) {
                                         counter++;
                                     }
-                                }
+
                             }
                         }
                     }
                     matriz[i][j] = counter;
                 }
-
             }
         }
 
@@ -85,14 +83,6 @@ public class MukiController {
         }
         mina.setPosiciones(matriz);
 
-        //Ahora seteamos estados como string
-        String[][] matrizEstado = new String[mina.getNumFilas()][mina.getNumColumnas()];
-        for(int i=0;  i<mina.getNumFilas(); i++){
-            for(int j=0;  j<mina.getNumColumnas(); j++) {
-                matrizEstado[i][j] = "Oculto";
-            }
-        }
-        mina.setEstadoMina(matrizEstado);
 
         model.addAttribute("mina",mina);
         return "juego";
@@ -118,21 +108,59 @@ public class MukiController {
         int columna = listaNum.get(1);
         int[][] matriz = mina.getPosiciones();
 
-        if(mina.getPosiciones()[fila][columna] !=20 ) {
+
+        if(mina.getPosiciones()[fila][columna] ==30) {
             //No escoge bomba
             //Recorremos alrededor de la casilla escogida
-            matriz[fila][columna]= matriz[fila][columna]*(-1);
-            for(int i= fila-1 ; i<=fila+1; i++ ){
-                for(int j= columna-1 ; j<=columna+1 ; j++){
-                    //Restriccion para los bordes de la matriz
-                    if((i>=0 && i<= mina.getNumFilas()-1) && (j>=0 && j<=mina.getNumColumnas()-1) && (i!=fila || j!=columna)){
-                        //Restriccion para detectar otra bomba
-                        if(matriz[i][j] >0 && matriz[i][j] !=20 ){
-                            matriz[i][j] =matriz[i][j]*(-1);
+
+            ArrayList<Integer> lista =new ArrayList<Integer>();
+            lista=mina.minar(mina, matriz, fila, columna);
+            ArrayList<Integer> aux= new ArrayList<>();
+            while(!lista.isEmpty()){
+                fila=lista.remove(0);
+                columna=lista.remove(0);
+                System.out.println("fila : " + fila);
+                System.out.println("columna : " + columna);
+                aux = mina.minar(mina,matriz,fila,columna);
+                if(!aux.isEmpty()) {
+                    ArrayList<String> listaCoordenadas = new ArrayList<>();
+                    for (int o = 1; o <= lista.size() / 2; o++) {
+                        String coordenadaGuardada = "" + lista.get(2 * o - 2) + lista.get(2 * o - 1);
+                        listaCoordenadas.add(coordenadaGuardada);
+                    }
+
+                    for (int e = 1; e <= aux.size() / 2; e++) {
+                        String coordenada = "" + aux.get(2 * e - 2) + aux.get(2 * e - 1);
+                        int auxCounter = 0;
+                        for(String uu : listaCoordenadas){
+                            if(uu.equals(coordenada)){
+                                auxCounter++;
+                            }
+                        }
+                        if (auxCounter != 0) {
+                            lista.add(aux.get(2 * e - 2));
+                            lista.add(aux.get(2 * e - 1));
                         }
                     }
                 }
             }
+
+/*
+                for(int j=1; j<=lista.size()/2 ; j++){
+                    int filaFila = lista.get(j*2-2);
+                    int columnaColumna = lista.get(j*2-1);
+                    listaLista.add(mina.minar(mina,matriz,filaFila,columnaColumna));
+                }
+
+                    ArrayList<ArrayList<Integer>> prime=  new ArrayList<>();
+                    for (ArrayList<Integer> l : listaLista) {
+                        for (int j = 1; j <= l.size() / 2; j++) {
+                            int filaFila = l.get(j * 2 - 2);
+                            int columnaColumna = l.get(j*2 - 1);
+                            prime.add(mina.minar(mina, matriz, filaFila, columnaColumna));
+                        }
+                    }
+*/
 
 
             mina.setPosiciones(matriz);
@@ -149,23 +177,42 @@ public class MukiController {
                     if(matriz[i][j] > 0){
                         celdasOcultas++;
                     }
-
                 }
             }
             //Puede ganar si solo quedan celdas de bombas
             if(celdasOcultas<=mina.getNumBombas()  && mina.getFallas()<mina.getNumIntentos()){
                 mina.setGano(true);
             }
-        }else{
+        } else if (mina.getPosiciones()[fila][columna] ==20  ) {
             //Escoge bomba
             matriz[fila][columna] = matriz[fila][columna]*(-1);
             mina.setPosiciones(matriz);
             mina.setFallas(mina.getFallas()+1);
-        }
+        }else{
+            matriz[fila][columna] = matriz[fila][columna] *(-1);
+            mina.setPosiciones(matriz);
+            //Verificacion por si es que gano
+            int celdasDescubiertas= 0;
+            int celdasOcultas= 0;
 
-        //LLAMEN A DIOS
+            for(int i =0;i<mina.getNumFilas() ; i++){
+                for(int j =0 ; j<mina.getNumColumnas(); j++){
+                    if(matriz[i][j] < 0){
+                        celdasDescubiertas++;
+                    }
+                    if(matriz[i][j] > 0){
+                        celdasOcultas++;
+                    }
+                }
+            }
+            //Puede ganar si solo quedan celdas de bombas
+            if(celdasOcultas<=mina.getNumBombas()  && mina.getFallas()<mina.getNumIntentos()){
+                mina.setGano(true);
+            }
+        }
         return "juego";
     }
 
 
 }
+
--- src/main/java/com/example/lab3_gtics/Entity/Mina.java
+++ src/main/java/com/example/lab3_gtics/Entity/Mina.java
@@ -16,4 +16,44 @@ public class Mina {
     private String[][] estadoMina;
     private int fallas = 0;
     private Boolean gano= false;
+
+
+    public ArrayList<Integer> minar(Mina mina, int [][] matrix , int fila , int columna ){
+        ArrayList<Integer> nuevosMinados = new ArrayList<>();
+        matrix[fila][columna]= matrix[fila][columna]*(-1);
+        for(int i= fila-1 ; i<=fila+1; i++ ){
+            for(int j= columna-1 ; j<=columna+1 ; j++){
+                //Restriccion para los bordes de la matriz
+                if((i>=0 && i<= mina.getNumFilas()-1) && (j>=0 && j<=mina.getNumColumnas()-1) && (i!=fila || j!=columna)){
+                    //Restriccion para detectar otra bomba
+                    if(matrix[i][j] >0 && matrix[i][j] !=20 ){
+                        if(matrix[i][j]==30) {
+                            nuevosMinados.add(i);
+                            nuevosMinados.add(j);
+                        }
+                        matrix[i][j] =matrix[i][j]*(-1);
+                    }
+                }
+            }
+        }
+        return nuevosMinados;
+    }
+    public ArrayList<Integer> minar2(Mina mina, int [][] matrix , int fila , int columna ){
+        ArrayList<Integer> nuevosMinados = new ArrayList<>();
+        matrix[fila][columna]= matrix[fila][columna]*(-1);
+        for(int i= fila-1 ; i<=fila+1; i++ ){
+            for(int j= columna-1 ; j<=columna+1 ; j++){
+                //Restriccion para los bordes de la matriz
+                if((i>=0 && i<= mina.getNumFilas()-1) && (j>=0 && j<=mina.getNumColumnas()-1) && (i!=fila || j!=columna)){
+                    //Restriccion para detectar otra bomba
+                    if(matrix[i][j] >0 && matrix[i][j] !=20 ){
+                        matrix[i][j] =matrix[i][j]*(-1);
+                        nuevosMinados.add(i);
+                        nuevosMinados.add(j);
+                    }
+                }
+            }
+        }
+        return nuevosMinados;
+    }
 }
